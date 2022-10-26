cb4f6a3cb6d432e2afddfccf32c07e832a70c47e
Le Duc Trung <leductrungxf@gmail.com>
2022-10-26 09:17:01 +0200

    Use jupyter-proxy

--- binder/jupyter_notebook_config.py
+++ binder/jupyter_notebook_config.py
@@ -1,3 +1,31 @@
 c.LabApp.collaborative = True
-c.LabApp.token = ''
-c.LabApp.ip = '*'
+c.LabApp.token = ""
+common = [
+    "--no-browser",
+    "--port={port}",
+    "--ServerApp.ip=127.0.0.1",
+    '--ServerApp.token=""',
+    # Disable dns rebinding protection here, since our 'Host' header
+    # is not going to be localhost when coming from hub.mybinder.org
+    "--ServerApp.allow_remote_access=True",
+]
+
+lab_command = " ".join(
+    [
+        "jupyter",
+        "lab",
+        "--collaborative",
+        "--ServerApp.base_url={base_url}cad-dev",
+    ]
+    + common
+)
+
+c.ServerProxy.servers = {
+    "cad-dev": {
+        "command": ["/bin/bash", "-c", lab_command],
+        "timeout": 60,
+        "absolute_url": True,
+    },
+}
+
+c.NotebookApp.default_url = "/cad-dev"
