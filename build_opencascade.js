aa69bd57c90a9481e7a267132297f408bc5835e4
martinRenou <martin.renou@gmail.com>
2022-12-06 14:55:37 +0100

    Automatically rebuild open cascade when needed

--- build_opencascade.js
+++ build_opencascade.js
@@ -1,10 +1,40 @@
-const { existsSync } = require('fs');
-const { execSync } = require('node:child_process');
+const { existsSync, readFileSync, writeFileSync } = require('fs');
+const crypto = require('crypto');
+const path = require('path');
+const { execSync } = require('child_process');
 
-if (!existsSync('src/worker/opencascade/jupytercad.opencascade.wasm')) {
-    const IMAGE_NAME = 'donalffons/opencascade.js:2.0.0-beta.371bbb0';
+const IMAGE_NAME = 'donalffons/opencascade.js:2.0.0-beta.371bbb0';
 
-    execSync(`docker pull ${IMAGE_NAME}`);
+const OPEN_CASCADE_DIR = path.join('src', 'worker', 'opencascade');
 
-    execSync(`docker run --rm -v "$(pwd):/src" -u "$(id -u):$(id -g)" ${IMAGE_NAME} build.yml`, { cwd: 'src/worker/opencascade' });
+const VERSION_FILE_NAME = 'jupytercad.opencascade.version';
+const VERSION_FILE_PATH = path.join(OPEN_CASCADE_DIR, VERSION_FILE_NAME);
+
+const hashSum = crypto.createHash('sha256');
+
+hashSum.update(readFileSync(path.join(OPEN_CASCADE_DIR, 'build.yml')));
+const buildHash = hashSum.digest('hex');
+
+let needsRebuild = true;
+
+if (existsSync(VERSION_FILE_PATH)) {
+  const currentHash = readFileSync(VERSION_FILE_PATH, {
+    encoding: 'utf8',
+    flag: 'r'
+  });
+
+  if (currentHash === buildHash) {
+    needsRebuild = false;
+  }
+}
+
+if (needsRebuild) {
+  execSync(`docker pull ${IMAGE_NAME}`);
+
+  execSync(
+    `docker run --rm -v "$(pwd):/src" -u "$(id -u):$(id -g)" ${IMAGE_NAME} build.yml`,
+    { cwd: OPEN_CASCADE_DIR }
+  );
+
+  writeFileSync(VERSION_FILE_PATH, buildHash);
 }
