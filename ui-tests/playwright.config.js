c63dcf2457543fe501a50410032e9fda514c7667
Duc Trung Le <leductrungxf@gmail.com>
2022-12-19 22:21:13 +0100

    Add retry

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -10,5 +10,7 @@ module.exports = {
     url: 'http://localhost:8888/lab',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI
-  }
+  },
+  // Try one retry as some tests are flaky
+  retries: 1
 };
