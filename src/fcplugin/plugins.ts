945b988c3018e3aaa413f489162a3726aaad446f
Carlos Herrero <contact@carloshb.com>
2023-01-25 13:06:24 +0100

    Add launcher buttons

--- src/fcplugin/plugins.ts
+++ src/fcplugin/plugins.ts
@@ -3,7 +3,17 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { IThemeManager, WidgetTracker } from '@jupyterlab/apputils';
+import {
+  ICommandPalette,
+  IThemeManager,
+  WidgetTracker
+} from '@jupyterlab/apputils';
+
+import { fileIcon } from '@jupyterlab/ui-components';
+
+import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
+
+import { ILauncher } from '@jupyterlab/launcher';
 
 import { JupyterCadWidgetFactory } from '../factory';
 import { IAnnotation, IJupyterCadDocTracker } from './../token';
@@ -12,11 +22,20 @@ import { IAnnotationModel, IJupyterCadWidget } from '../types';
 
 const FACTORY = 'Jupytercad Freecad Factory';
 
+const PALETTE_CATEGORY = 'JupyterCAD';
+
+namespace CommandIDs {
+  export const createNew = 'jupytercad:create-new-FCStd-file';
+}
+
 const activate = (
   app: JupyterFrontEnd,
   tracker: WidgetTracker<IJupyterCadWidget>,
   themeManager: IThemeManager,
-  annotationModel: IAnnotationModel
+  annotationModel: IAnnotationModel,
+  browserFactory: IFileBrowserFactory,
+  launcher: ILauncher | null,
+  palette: ICommandPalette | null,
 ): void => {
   // Creating the widget factory to register it so the document manager knows about
   // our new DocumentWidget
@@ -58,11 +77,65 @@ const activate = (
     app.shell.activateById('jupytercad::leftControlPanel');
     app.shell.activateById('jupytercad::rightControlPanel');
   });
+
+  app.commands.addCommand(CommandIDs.createNew, {
+    label: args => args['isPalette'] ? 'New FCStd Editor' : 'FCStd Editor',
+    caption: 'Create a new FCStd Editor',
+    icon: args => (args['isPalette'] ? undefined : fileIcon),
+    execute: async args => {
+      // Get the directory in which the FCStd file must be created;
+      // otherwise take the current filebrowser directory
+      const cwd = (args['cwd'] || browserFactory.tracker.currentWidget?.model.path) as string;
+
+      // Create a new untitled Blockly file
+      let model = await app.serviceManager.contents.newUntitled({
+        path: cwd,
+        type: 'file',
+        ext: '.FCStd'
+      });
+
+      console.debug("Model:", model);
+      model = await app.serviceManager.contents.save(
+        model.path,
+        {
+          ...model,
+          format: 'base64',
+          size: undefined,
+          content: btoa("")
+        }
+      );
+
+      // Open the newly created file with the 'Editor'
+      return app.commands.execute('docmanager:open', {
+        path: model.path,
+        factory: FACTORY
+      });
+    }
+  });
+
+  // Add the command to the launcher
+  if (launcher) {
+    launcher.add({
+      command: CommandIDs.createNew,
+      category: 'Other',
+      rank: 1
+    });
+  }
+
+  // Add the command to the palette
+  if (palette) {
+    palette.addItem({
+      command: CommandIDs.createNew,
+      args: { isPalette: true },
+      category: PALETTE_CATEGORY
+    });
+  }
 };
 
 const fcplugin: JupyterFrontEndPlugin<void> = {
   id: 'jupytercad:fcplugin',
-  requires: [IJupyterCadDocTracker, IThemeManager, IAnnotation],
+  requires: [IJupyterCadDocTracker, IThemeManager, IAnnotation, IFileBrowserFactory],
+  optional: [ILauncher, ICommandPalette],
   autoStart: true,
   activate
 };
