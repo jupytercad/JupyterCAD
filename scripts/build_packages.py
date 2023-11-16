fdfa9befbae138640a6bc3418f50c3a9a3b9a414
Duc Trung Le <leductrungxf@gmail.com>
2023-11-16 18:19:42 +0100

    Update CI

---
+++ scripts/build_packages.py
@@ -0,0 +1,24 @@
+import subprocess
+from pathlib import Path
+
+
+def execute(cmd: str, cwd=None):
+    subprocess.run(cmd.split(" "), check=True, cwd=cwd)
+
+
+def build_packages():
+    root_path = Path(__file__).parents[1]
+    requirements_build_path = root_path / "requirements-build.txt"
+    install_build_deps = f"python -m pip install -r {requirements_build_path}"
+
+    python_package_prefix = "python"
+    python_packages = ["jupytercad-core", "jupytercad-lab", "jupytercad-app"]
+
+    execute(install_build_deps)
+
+    for py_package in python_packages:
+        execute("python -m build", cwd=root_path / python_package_prefix / py_package)
+
+
+if __name__ == "__main__":
+    build_packages()
