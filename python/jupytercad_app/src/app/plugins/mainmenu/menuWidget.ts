b82e7f3b1615e0d90a00939443314ab6c67fe577
Duc Trung Le <leductrungxf@gmail.com>
2024-06-19 12:27:20 +0200

    Typo

--- python/jupytercad_app/src/app/plugins/mainmenu/menuWidget.ts
+++ python/jupytercad_app/src/app/plugins/mainmenu/menuWidget.ts
@@ -22,7 +22,7 @@ export class MainMenu extends MenuBar {
     this._commands.addCommand('jupytercad:help-menu:documentation', {
       label: 'Documentation',
       execute: () => {
-        window.open('https://github.com/jupytercad/jupyterCAD', '_blank');
+        window.open('https://github.com/jupytercad/JupyterCAD', '_blank');
       },
       icon: homeIcon
     });
