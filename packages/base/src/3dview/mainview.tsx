be701cc36b8530837b956420e27adf532bf12cc2
arjxn-py <arjxn.py@gmail.com>
2024-11-28 23:40:30 +0530

    fix rebasing issue

--- packages/base/src/3dview/mainview.tsx
+++ packages/base/src/3dview/mainview.tsx
@@ -426,6 +426,16 @@ export class MainView extends React.Component<IProps, IStates> {
         const updatedQuaternion = new THREE.Quaternion();
         updatedObject.getWorldQuaternion(updatedQuaternion);
 
+        const s = Math.sqrt(1 - updatedQuaternion.w * updatedQuaternion.w);
+          const updatedRotation = [
+            [
+              parseFloat((updatedQuaternion.x / s).toFixed(10)),
+              parseFloat((updatedQuaternion.y / s).toFixed(10)),
+              parseFloat((updatedQuaternion.z / s).toFixed(10))
+            ],
+            parseFloat((2 * Math.acos(updatedQuaternion.w) * (180 / Math.PI)).toFixed(10))
+          ];
+
         const obj = this._model.sharedModel.getObjectByName(objectName);
 
         if (obj && obj.parameters && obj.parameters.Placement) {
