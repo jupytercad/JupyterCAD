b5b60293f972936f32e081f9042da040d9ebfc46
Arjun Verma <arjxn.py@gmail.com>
2024-11-29 11:34:15 +0530

    Rotation Controls (#603)
    * undo opencascade positioning and rotation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * use Math.PI
    
    * Recentering done
    
    * Translation is smooth
    
    * Apply suggestions from code review
    
    Co-authored-by: martinRenou <martin.renou@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add helping comment
    
    * Try fixing exploded view
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * create interface `IMeshGroupMetadata`  and assign originalPosition in buildShape
    
    * Rotation works
    
    * refactor exploded view logic
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * only `newGeometryCenter` is not right
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * expand boundingGroup via meshGroup
    
    * Refactoring
    
    * Merge review suggestions
    
    * `getQuaternion` method
    
    * fallback not needed
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * rebase issue
    
    * more refactoring
    
    * Remove originalPosition refactor groupMetadata
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * in theory, this seems correct
    
    * rebase issue fixes
    
    * Update packages/base/src/3dview/mainview.tsx
    
    Co-authored-by: martinRenou <martin.renou@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * prevent default enabling transform controls
    
    * fix rebasing issue
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update packages/base/src/3dview/mainview.tsx
    
    Co-authored-by: martinRenou <martin.renou@gmail.com>
    
    * Add fallback for updatedRotation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: martinRenou <martin.renou@gmail.com>

--- packages/base/src/3dview/mainview.tsx
+++ packages/base/src/3dview/mainview.tsx
@@ -423,6 +423,24 @@ export class MainView extends React.Component<IProps, IStates> {
 
         const updatedPosition = new THREE.Vector3();
         updatedObject.getWorldPosition(updatedPosition);
+        const updatedQuaternion = new THREE.Quaternion();
+        updatedObject.getWorldQuaternion(updatedQuaternion);
+
+        const s = Math.sqrt(1 - updatedQuaternion.w * updatedQuaternion.w);
+
+        let updatedRotation;
+        if (s > 1e-6) {
+          updatedRotation = [
+            [
+              updatedQuaternion.x / s,
+              updatedQuaternion.y / s,
+              updatedQuaternion.z / s
+            ],
+            2 * Math.acos(updatedQuaternion.w) * (180 / Math.PI)
+          ];
+        } else {
+          updatedRotation = [[0, 0, 1], 0];
+        }
 
         const obj = this._model.sharedModel.getObjectByName(objectName);
 
@@ -437,7 +455,9 @@ export class MainView extends React.Component<IProps, IStates> {
             ...obj.parameters,
             Placement: {
               ...obj.parameters.Placement,
-              Position: newPosition
+              Position: newPosition,
+              Axis: updatedRotation[0],
+              Angle: updatedRotation[1]
             }
           });
         }
@@ -724,18 +744,21 @@ export class MainView extends React.Component<IProps, IStates> {
 
   private _onKeyDown(event: KeyboardEvent) {
     // TODO Make these Lumino commands? Or not?
-    if (this._clipSettings.enabled) {
-      switch (event.key) {
-        case 'r':
-          event.preventDefault();
-          event.stopPropagation();
+    if (this._clipSettings.enabled || this._transformControls.enabled) {
+      const toggleMode = (control: any) => {
+        control.setMode(control.mode === 'rotate' ? 'translate' : 'rotate');
+      };
 
-          if (this._clipPlaneTransformControls.mode === 'rotate') {
-            this._clipPlaneTransformControls.setMode('translate');
-          } else {
-            this._clipPlaneTransformControls.setMode('rotate');
-          }
-          break;
+      if (event.key === 'r' && this._clipSettings.enabled) {
+        event.preventDefault();
+        event.stopPropagation();
+        toggleMode(this._clipPlaneTransformControls);
+      }
+
+      if (event.key === 'r' && this._transformControls.enabled) {
+        event.preventDefault();
+        event.stopPropagation();
+        toggleMode(this._transformControls);
       }
     }
   }
