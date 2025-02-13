fb68f00434ed49768c45bad6195f1dfcce077faf
arjxn-py <arjxn.py@gmail.com>
2025-02-13 21:01:19 +0530

    could this be it?

--- ui-tests/playwright-lite.config.js
+++ ui-tests/playwright-lite.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ...baseConfig,
   webServer: {
     command: 'jlpm start:lite',
-    url: 'http://localhost:8888/lab',
+    url: 'http://localhost:8000/lab',
     timeout: 120 * 1000,
     reuseExistingServer: false
   },
