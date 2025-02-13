ea7524930fa939547ed6e130c8fc3423f1c6796d
arjxn-py <arjxn.py@gmail.com>
2025-02-13 15:56:18 +0530

    Configure lite tests

---
+++ ui-tests/playwright-lite.config.js
@@ -0,0 +1,25 @@
+/**
+ * Configuration for Playwright using default from @jupyterlab/galata
+ */
+const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
+
+module.exports = {
+  ...baseConfig,
+  webServer: {
+    command: 'jlpm start:lite',
+    url: 'http://localhost:8866/lab',
+    timeout: 120 * 1000,
+    reuseExistingServer: false
+  },
+  retries: 1,
+  use: {
+    ...baseConfig.use,
+    trace: 'off',
+
+  },
+  expect: {
+    toMatchSnapshot: {
+      maxDiffPixelRatio: 0.002,
+    },
+  },
+};
