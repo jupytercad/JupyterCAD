326c32ad1f889417aab22656b216294846fee96f
arjxn-py <arjxn.py@gmail.com>
2025-02-13 19:13:53 +0530

    run lite tests on 8888 only

--- ui-tests/playwright-lite.config.js
+++ ui-tests/playwright-lite.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ...baseConfig,
   webServer: {
     command: 'jlpm start:lite',
-    url: 'http://localhost:8866/lab',
+    url: 'http://localhost:8888/lab',
     timeout: 120 * 1000,
     reuseExistingServer: false
   },
