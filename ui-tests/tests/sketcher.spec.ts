01561dbdc2ee96da3db80019119cdc6a031a490b
arjxn-py <arjxn.py@gmail.com>
2025-02-14 17:35:14 +0530

    just leave one test to run for now

--- ui-tests/tests/sketcher.spec.ts
+++ ui-tests/tests/sketcher.spec.ts
@@ -1,100 +1,100 @@
-import { expect, test, galata } from '@jupyterlab/galata';
-import path from 'path';
+// import { expect, test, galata } from '@jupyterlab/galata';
+// import path from 'path';
 
-test.use({ autoGoto: false });
+// test.use({ autoGoto: false });
 
-test.describe('Sketcher test', () => {
-  let errors = 0;
-  test.beforeEach(async ({ page, request }) => {
-    page.setViewportSize({ width: 1920, height: 1080 });
-    page.on('console', message => {
-      if (message.type() === 'error') {
-        console.log('ERROR MSG', message.text());
-        errors += 1;
-      }
-    });
+// test.describe('Sketcher test', () => {
+//   let errors = 0;
+//   test.beforeEach(async ({ page, request }) => {
+//     page.setViewportSize({ width: 1920, height: 1080 });
+//     page.on('console', message => {
+//       if (message.type() === 'error') {
+//         console.log('ERROR MSG', message.text());
+//         errors += 1;
+//       }
+//     });
 
-    const content = galata.newContentsHelper(request);
-    await content.deleteDirectory('/examples');
-    await content.uploadDirectory(
-      path.resolve(__dirname, '../../examples'),
-      '/examples'
-    );
-  });
+//     const content = galata.newContentsHelper(request);
+//     await content.deleteDirectory('/examples');
+//     await content.uploadDirectory(
+//       path.resolve(__dirname, '../../examples'),
+//       '/examples'
+//     );
+//   });
 
-  test.afterEach(async ({ page }) => {
-    errors = 0;
-  });
+//   test.afterEach(async ({ page }) => {
+//     errors = 0;
+//   });
 
-  test(`Should open the sketcher dialog`, async ({ page }) => {
-    await page.goto();
+//   test(`Should open the sketcher dialog`, async ({ page }) => {
+//     await page.goto();
 
-    const fileName = 'test.jcad';
-    await page.getByTitle('Create a new JCAD Editor').first().click();
-    await page.locator('div.jpcad-Spinner').waitFor({ state: 'hidden' });
+//     const fileName = 'test.jcad';
+//     await page.getByTitle('Create a new JCAD Editor').first().click();
+//     await page.locator('div.jpcad-Spinner').waitFor({ state: 'hidden' });
 
-    await page.waitForTimeout(1000);
-    if (await page.getByRole('button', { name: 'Ok' }).isVisible()) {
-      await page.getByRole('button', { name: 'Ok' }).click();
-    }
-    // Close the property panel
-    await page
-      .getByRole('tablist', { name: 'alternate sidebar' })
-      .getByRole('tab', { name: 'JupyterCad Control Panel' })
-      .click();
+//     await page.waitForTimeout(1000);
+//     if (await page.getByRole('button', { name: 'Ok' }).isVisible()) {
+//       await page.getByRole('button', { name: 'Ok' }).click();
+//     }
+//     // Close the property panel
+//     await page
+//       .getByRole('tablist', { name: 'alternate sidebar' })
+//       .getByRole('tab', { name: 'JupyterCad Control Panel' })
+//       .click();
 
-    await page.getByTitle('New Sketch').click();
-    const dialog = await page.$('.lm-Widget.lm-Panel.jp-Dialog-content');
-    if (dialog) {
-      expect(await dialog.screenshot()).toMatchSnapshot({
-        name: `Sketcher-Display-${fileName}.png`,
-        maxDiffPixelRatio: 0.01
-      });
-    }
-  });
+//     await page.getByTitle('New Sketch').click();
+//     const dialog = await page.$('.lm-Widget.lm-Panel.jp-Dialog-content');
+//     if (dialog) {
+//       expect(await dialog.screenshot()).toMatchSnapshot({
+//         name: `Sketcher-Display-${fileName}.png`,
+//         maxDiffPixelRatio: 0.01
+//       });
+//     }
+//   });
 
-  test(`Should draw a circle`, async ({ page }) => {
-    await page.goto();
-    const fileName = 'test.jcad';
-    await page.getByTitle('Create a new JCAD Editor').first().click();
-    await page.locator('div.jpcad-Spinner').waitFor({ state: 'hidden' });
+//   test(`Should draw a circle`, async ({ page }) => {
+//     await page.goto();
+//     const fileName = 'test.jcad';
+//     await page.getByTitle('Create a new JCAD Editor').first().click();
+//     await page.locator('div.jpcad-Spinner').waitFor({ state: 'hidden' });
 
-    await page.waitForTimeout(1000);
-    if (await page.getByRole('button', { name: 'Ok' }).isVisible()) {
-      await page.getByRole('button', { name: 'Ok' }).click();
-    }
-    // Close the property panel
-    await page
-      .getByRole('tablist', { name: 'alternate sidebar' })
-      .getByRole('tab', { name: 'JupyterCad Control Panel' })
-      .click();
+//     await page.waitForTimeout(1000);
+//     if (await page.getByRole('button', { name: 'Ok' }).isVisible()) {
+//       await page.getByRole('button', { name: 'Ok' }).click();
+//     }
+//     // Close the property panel
+//     await page
+//       .getByRole('tablist', { name: 'alternate sidebar' })
+//       .getByRole('tab', { name: 'JupyterCad Control Panel' })
+//       .click();
 
-    await page.getByTitle('New Sketch').click();
-    await page.getByRole('button', { name: 'CIRCLE' }).click();
-    await page
-      .locator('canvas')
-      .nth(1)
-      .click({
-        position: {
-          x: 455,
-          y: 209
-        }
-      });
-    await page
-      .locator('canvas')
-      .nth(1)
-      .click({
-        position: {
-          x: 455,
-          y: 335
-        }
-      });
-    const dialog = await page.$('.lm-Widget.lm-Panel.jp-Dialog-content');
-    if (dialog) {
-      expect(await dialog.screenshot()).toMatchSnapshot({
-        name: `Sketcher-Circle-${fileName}.png`,
-        maxDiffPixelRatio: 0.01
-      });
-    }
-  });
-});
+//     await page.getByTitle('New Sketch').click();
+//     await page.getByRole('button', { name: 'CIRCLE' }).click();
+//     await page
+//       .locator('canvas')
+//       .nth(1)
+//       .click({
+//         position: {
+//           x: 455,
+//           y: 209
+//         }
+//       });
+//     await page
+//       .locator('canvas')
+//       .nth(1)
+//       .click({
+//         position: {
+//           x: 455,
+//           y: 335
+//         }
+//       });
+//     const dialog = await page.$('.lm-Widget.lm-Panel.jp-Dialog-content');
+//     if (dialog) {
+//       expect(await dialog.screenshot()).toMatchSnapshot({
+//         name: `Sketcher-Circle-${fileName}.png`,
+//         maxDiffPixelRatio: 0.01
+//       });
+//     }
+//   });
+// });
