500f4381f6b080284f8d2b260098775a3959c9b2
martinRenou <martin.renou@gmail.com>
2025-05-06 17:12:25 +0200

    Properly handle lumino events (#735)
    * Properly handle lumino events
    
    * Fixing when switching camera proj

--- packages/base/src/3dview/mainviewmodel.ts
+++ packages/base/src/3dview/mainviewmodel.ts
@@ -52,6 +52,14 @@ export class MainViewModel implements IDisposable {
     return this._renderSignal;
   }
 
+  get afterShowSignal(): ISignal<this, null> {
+    return this._afterShowSignal;
+  }
+
+  emitAfterShow() {
+    this._afterShowSignal.emit(null);
+  }
+
   get workerBusy(): ISignal<this, boolean> {
     return this._workerBusy;
   }
@@ -322,6 +330,7 @@ export class MainViewModel implements IDisposable {
       postResult?: IDict<IPostOperatorInput>;
     }
   >(this);
+  private _afterShowSignal = new Signal<this, null>(this);
   private _workerBusy = new Signal<this, boolean>(this);
   private _isDisposed = false;
 }
