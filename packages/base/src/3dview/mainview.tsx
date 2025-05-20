21e4c09b0f9c06a0e1ce9aa4f2e30443e1f73c5e
arjxn-py <arjxn.py@gmail.com>
2025-05-20 16:36:04 +0530

    Start with camera from settings and update camera params in updatereflength

--- packages/base/src/3dview/mainview.tsx
+++ packages/base/src/3dview/mainview.tsx
@@ -25,7 +25,7 @@ import { ViewHelper } from 'three/examples/jsm/helpers/ViewHelper';
 
 import { FloatingAnnotation } from '../annotation';
 import { getCSSVariableColor, throttle } from '../tools';
-import { ClipSettings, ExplodedView, SplitScreenSettings } from '../types';
+import { CameraSettings, ClipSettings, ExplodedView, SplitScreenSettings } from '../types';
 import { FollowIndicator } from './followindicator';
 import {
   BasicMesh,
@@ -249,12 +249,28 @@ export class MainView extends React.Component<IProps, IStates> {
       SPLITVIEW_BACKGROUND_COLOR.set(
         getCSSVariableColor(SPLITVIEW_BACKGROUND_COLOR_CSS)
       );
-      this._camera = new THREE.PerspectiveCamera(
-        50,
-        2,
-        CAMERA_NEAR,
-        CAMERA_FAR
-      );
+      if (this._mainViewModel.viewSettings.cameraSettings) {
+        const cameraSettings = this._mainViewModel.viewSettings
+          .cameraSettings as CameraSettings;
+        if (cameraSettings.type === 'Perspective') {
+          this._camera = new THREE.PerspectiveCamera(
+            50,
+            2,
+            CAMERA_NEAR,
+            CAMERA_FAR
+          );
+        } else if (cameraSettings.type === 'Orthographic') {
+          const width = this._divRef.current?.clientWidth || 0;
+          const height = this._divRef.current?.clientHeight || 0;
+          this._camera = new THREE.OrthographicCamera(
+            width / -2,
+            width / 2,
+            height / 2,
+            height / -2
+          );
+          this._camera.updateProjectionMatrix();
+        }
+      }
       this._camera.position.set(8, 8, 8);
       this._camera.up.set(0, 0, 1);
 
@@ -1044,6 +1060,25 @@ export class MainView extends React.Component<IProps, IStates> {
         );
       }
 
+      // Update near and far for orthographic camera if applicable
+      if (
+        this._camera instanceof THREE.OrthographicCamera
+      ) {
+        const near = Math.max(this._refLength / 20, 0.01);
+        const far = this._refLength * 20;
+
+        const distance = this._camera.position.distanceTo(this._controls.target);
+        const zoomFactor = 1000 / distance;
+        this._camera.zoom = zoomFactor;
+
+        console.log(near, far);
+        
+
+        this._camera.near = near;
+        this._camera.far = far;
+        this._camera.updateProjectionMatrix();
+      }
+
       // Update clip plane size
       this._clippingPlaneMeshControl.geometry = new THREE.PlaneGeometry(
         this._refLength * 10,
@@ -1054,8 +1089,6 @@ export class MainView extends React.Component<IProps, IStates> {
     } else {
       this._refLength = null;
     }
-
-    this._updateCamera();
   }
 
   private async _objToMesh(
