c02306d28be067f2e5e3ef7d04672e790f1582d6
asmith26 <asmith26@users.noreply.github.com>
2025-10-30 16:39:54 +0000

    Points as sphere

--- packages/occ-worker/src/occapi/geomPoint.ts
+++ packages/occ-worker/src/occapi/geomPoint.ts
@@ -1,15 +1,17 @@
 import { IGeomPoint } from '@jupytercad/schema';
-import { getOcc } from './common';
+import { getOcc, setShapePlacement } from './common';
 
 export function _GeomPoint(arg: IGeomPoint): any {
   const oc = getOcc();
-  const point = new oc.gp_Pnt_3(arg.X, arg.Y, arg.Z);
-
-  // create a very short edge to represent the point visually
-  const eps = 1e-3;
-  const point2 = new oc.gp_Pnt_3(arg.X + eps, arg.Y, arg.Z);
-  const edge = new oc.BRepBuilderAPI_MakeEdge_3(point, point2).Edge();
-  const pointWire = new oc.BRepBuilderAPI_MakeWire_2(edge).Wire();
-
-  return pointWire;
+  // Create a small sphere for visibility
+  const radius = 1e-3;
+  const sphere = new oc.BRepPrimAPI_MakeSphere_4(radius, 0, 0, 0);
+  const shape = sphere.Shape();
+  // Position the sphere at the sketch point coordinates
+  const placement = {
+    Position: [arg.X, arg.Y, arg.Z],
+    Axis: [0, 0, 1],
+    Angle: 0
+  };
+  return setShapePlacement(shape, placement);
 }
